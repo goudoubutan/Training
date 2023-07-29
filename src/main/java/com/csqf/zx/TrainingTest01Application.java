3bc9abe7c6f58f22d44b389e2968091759140ef9
xinforever <1182638329@qq.com>
2023-07-29 14:45:27 +0800

    XinForever练习demo

--- src/main/java/com/csqf/zx/TrainingTest01Application.java
+++ src/main/java/com/csqf/zx/TrainingTest01Application.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+//@CrossOrigin(origins = "http://localhost:8080",maxAge = 3600)
 @SpringBootApplication
 @MapperScan("com.csqf.zx.mapper")
 public class TrainingTest01Application {
